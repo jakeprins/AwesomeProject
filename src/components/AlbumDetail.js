cfc9fe1396fa3162c6e301ba9e70ab7c86edc452
Jake Prins <jakeprins@outlook.com>
2016-11-15 17:38:12 +0100

    adds link to buttons

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import { Text, View, Image } from 'react-native';
+import { Text, View, Image, Linking } from 'react-native';
 import Card from './Card';
 import CardSection from './CardSection';
+import Button from './Button';
 
 const AlbumDetail = ({ album }) => {
-  const { title, artist, thumbnail_image, image } = album;
+  const { title, artist, thumbnail_image, image, url } = album;
   const {
     thumbnailStyle,
     headerContentStyle,
@@ -16,22 +17,27 @@ const AlbumDetail = ({ album }) => {
   return (
     <Card>
       <CardSection>
-      <View style={thumbnailContainerStyle}>
-        <Image
-          style={thumbnailStyle}
-          source={{ uri: thumbnail_image }}
-          />
-      </View>
-      <View style={headerContentStyle}>
-        <Text style={headerTextStyle}>{title}</Text>
-        <Text>{artist}</Text>
-      </View>
+        <View style={thumbnailContainerStyle}>
+          <Image
+            style={thumbnailStyle}
+            source={{ uri: thumbnail_image }}
+            />
+        </View>
+        <View style={headerContentStyle}>
+          <Text style={headerTextStyle}>{title}</Text>
+          <Text>{artist}</Text>
+        </View>
       </CardSection>
+
       <CardSection>
         <Image
         source={{ uri: image }}
         style={imageStyle}
         />
+      </CardSection>
+
+      <CardSection>
+        <Button onPress={() => Linking.openURL(url)} />
       </CardSection>
     </Card>
   );
